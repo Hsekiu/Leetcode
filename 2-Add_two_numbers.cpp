fec163bb836610aa135e43a877f039d7200536f3
Hsekiu <kpetr7@gmail.com>
2017-09-02 12:54:06 -0700

    Added semi complete problem 2.

---
+++ 2-Add_two_numbers.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+//Solves problem for inputs less then size of int
+//Todo: reimplement adding to just using lists.
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode* solution = new ListNode(0);
+        ListNode* head = solution;
+        
+        int total = listToInt(l1) + listToInt(l2);
+        if (total == 0) {
+            return head;
+        }
+        std::cout << listToInt(l1) << "and: " << listToInt(l2) << std::enld;
+        
+        int i = 0;
+        while (total != 0) {
+            solution->next = new ListNode(total%10);
+            solution = solution->next;
+            total = (total - total%10) / 10;
+            i++;
+        }
+        
+        return head->next;
+    }
+    
+    int listToInt(ListNode* l) {
+        int power = 0;
+        int tmp = 0;
+        while (l != 0) {
+            tmp += (l->val)*(pow(10,power));
+            power++;
+            l = l->next;
+        }
+        return tmp;
+    }
+};
