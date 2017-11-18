02394e7064ef416ee4efe6cbd6ff9de1806bcad9
Hsekiu <kpetr7@gmail.com>
2017-11-17 21:54:32 -0800

    Fixed solution #9b.

--- 9-Palindrome-number_b.cpp
+++ 9-Palindrome-number_b.cpp
@@ -14,41 +14,45 @@ public:
     
     bool isPalindrome(int x) {
         
+        //If negative number then it's not a palindrome
         if(x < 0) {
             return false;
         }
         
         int size = getSize(x);
 
-        int a = x / (pow(10, size-1));
-        int b = x % 10;
-        bool flag = false;
+        //A is left number b is right number.
+        int a, b;
         
         for(int i = 0; i < size; i++) {
-            if(size < 2) {
+            
+            //If size is 1 or value is zero at this point in algorithm then must be palindrome.
+            if(size < 2 || x == 0) {
                 return true;
             }
             
+            //Occurs when zero's in front get cancelled out. 
             if(size != getSize(x)) {
                 if(getSize(x) == 1) {
                     return false;
                 }
-                std::cout << "bad size" << std::endl;
-                b = 0;
-                flag = true;
+                //std::cout << "bad size of: " << size - getSize(x) << std::endl;
+                a = 0;
             } else {
-                b = x % (int)(pow(10, i + 1));
+                a = x / (pow(10, ((getSize(x) - 1) - i)));
             }
-            a = x / (pow(10, ((getSize(x) - 1) - i)));
             
+            b = x % (int)(pow(10, i + 1));
+            
+            //Set X to value without head or tail numbers.
             x -= b * pow(10, getSize(x) - 1 - i);
             x /= 10;
             
-            if(!flag) {
-                size -= 2;
-                flag = false;
-            }
-            std::cout << "a is: " << a << " b is : " << b << " and x is: " << x << std::endl;
+            i--;
+
+            size -= 2;
+            
+            //std::cout << "a is: " << a << " b is : " << b << " and x is: " << x << std::endl;
             if (a != b) {
                 return false;
             }
